ebf39602af131e72683de6e176b4265a5422191f
LeNgan1411 <57651017+LeNgan1411@users.noreply.github.com>
2021-03-30 10:08:01 +0700

    sua

--- code/main.js
+++ code/main.js
@@ -1,72 +1,80 @@
 
 
 $(document).ready(function () {
-    $(".bt_login").click(function () { 
+    $(".login button").click(function () { 
         // console.log("đã click")
         $(".login-menu").removeClass("d-none");
+        $(".login").addClass("d-none");
     });
 });
 
-const accounts = [
-    {
-        name: "Lê Thị Ngân",
-        userName: "ngan1420",
-        password: "lengan",
-        img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
-    },
-    {
-        name: "Lê Thị A",
-        userName: "ngana1420",
-        password: "lengan",
-        img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
-    },
-    {
-        name: "Lê Thị B",
-        userName: "nganb1420",
-        password: "lengan",
-        img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
-    },
-    {
-        name: "Lê Thị C",
-        userName: "nganc1420",
-        password: "lengan",
-        img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
-    },
-];
+// const accounts = [
+//     {
+//         name: "Lê Thị Ngân",
+//         userName: "ngan1420",
+//         password: "lengan",
+//         img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
+//     },
+//     {
+//         name: "Lê Thị A",
+//         userName: "ngana1420",
+//         password: "lengan",
+//         img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
+//     },
+//     {
+//         name: "Lê Thị B",
+//         userName: "nganb1420",
+//         password: "lengan",
+//         img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
+//     },
+//     {
+//         name: "Lê Thị C",
+//         userName: "nganc1420",
+//         password: "lengan",
+//         img: "https://cdn6.f-cdn.com/contestentries/1458359/21383270/5c0fd7001d5ad_thumb900.jpg"
+//     },
+// ];
 
 
+// $(document).ready(function () {
+//     var user;
+//     var pass;
+//     // click nút đăng nhập
+//     $(".confirm button").click(function () { 
+//         // lấy giá trị trong ô nhập 
+//         user = $("#user").val();
+//         pass = $("#pass").val();
+//         if(user != null && pass != null && user.length > 0 && pass.length > 0){
+//             console.log("user:" + user + " pass:" + pass ); 
+//             console.log(login(user, pass).flag);
+//             if(login(user,pass).flag){
+//                 // thực hiện các tác vụ khi đn thành công
+//                 location= "https://www.google.com.vn/?hl=vi"
+//                 console.log("đã đăng nhập đúng")
+//             } else{
+//                 // thực hiện tác vụ khi đăng nhập ko thành công
+//                 $(".login-fail").removeClass("d-none");
+//             }
+//         }
+//     });
+
+//     function login(user, pass){
+//         var account;
+//         var flag = false;
+//         for (let i = 0; i < accounts.length; i++) {
+//             if(user == accounts[i].userName && pass == accounts[i].password){
+//                 account = accounts[i];
+//                 flag = true;
+//                 break;
+//             }
+//         }
+//         return {account, flag};
+//     }
+// });
 $(document).ready(function () {
-    var user;
-    var pass;
-    // click nút đăng nhập
-    $(".confirm button").click(function () { 
-        // lấy giá trị trong ô nhập 
-        user = $("#user").val();
-        pass = $("#pass").val();
-        if(user != null && pass != null && user.length > 0 && pass.length > 0){
-            console.log("user:" + user + " pass:" + pass ); 
-            console.log(login(user, pass).flag);
-            if(login(user,pass).flag){
-                // thực hiện các tác vụ khi đn thành công
-                location= "https://www.google.com.vn/?hl=vi"
-                console.log("đã đăng nhập đúng")
-            } else{
-                // thực hiện tác vụ khi đăng nhập ko thành công
-                $(".login-fail").removeClass("d-none");
-            }
-        }
+    $(".login-fail .cancel").click(function () { 
+        // console.log("đã click")
+        $(".login-fail").addClass("d-none");
+        $(".login-menu input").val("");
     });
-
-    function login(user, pass){
-        var account;
-        var flag = false;
-        for (let i = 0; i < accounts.length; i++) {
-            if(user == accounts[i].userName && pass == accounts[i].password){
-                account = accounts[i];
-                flag = true;
-                break;
-            }
-        }
-        return {account, flag};
-    }
 });
