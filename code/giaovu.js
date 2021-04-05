a80a9abcb772589e3388b733690e6b67f1dd0a15
LeNgan1411 <57651017+LeNgan1411@users.noreply.github.com>
2021-04-05 19:07:04 +0700

    giaovu

--- code/giaovu.js
+++ code/giaovu.js
@@ -4,3 +4,21 @@ $(document).ready(function () {
         $(".user-info").toggleClass("nn");
     });   
 });
+$(document).ready(function () {
+    $(".list-active button").click(function () { 
+        // console.log("đã click")
+        $(".tb").addClass("d-none");
+        $(".list-active button").addClass("d-none");
+        $(".list-active .update-tb").removeClass("d-none");
+        $(".list-active .x").removeClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $(".list-active .x").click(function () { 
+        // console.log("đã click")
+        $(".list-active button").removeClass("d-none");
+        $(".list-active .tb").removeClass("d-none");
+        $(".list-active .x").addClass("d-none");
+        $(".list-active .update-tb").addClass("d-none");
+    });
+});
