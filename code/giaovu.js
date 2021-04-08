6bba339737bb050dfc3a24a377580b3ccb1f0d07
LeNgan1411 <57651017+LeNgan1411@users.noreply.github.com>
2021-04-08 11:27:25 +0700

    giang vien

--- code/giaovu.js
+++ code/giaovu.js
@@ -5,20 +5,74 @@ $(document).ready(function () {
     });   
 });
 $(document).ready(function () {
-    $(".list-active button").click(function () { 
+    $("li.time-table .list-active button").click(function () { 
         // console.log("đã click")
-        $(".tb").addClass("d-none");
-        $(".list-active button").addClass("d-none");
-        $(".list-active .update-tb").removeClass("d-none");
-        $(".list-active .x").removeClass("d-none");
+        $("li.time-table .tb").addClass("d-none");
+        $("li.time-table .list-active button").addClass("d-none");
+        $("li.time-table .list-active .update-tb").removeClass("d-none");
+        $("li.time-table .list-active .x").removeClass("d-none");
     });
 });
 $(document).ready(function () {
-    $(".list-active .x").click(function () { 
+    $("li.time-table .list-active .x").click(function () { 
         // console.log("đã click")
-        $(".list-active button").removeClass("d-none");
-        $(".list-active .tb").removeClass("d-none");
-        $(".list-active .x").addClass("d-none");
-        $(".list-active .update-tb").addClass("d-none");
+        $("li.time-table .list-active button").removeClass("d-none");
+        $("li.time-table .list-active .tb").removeClass("d-none");
+        $("li.time-table .list-active .x").addClass("d-none");
+        $("li.time-table .list-active .update-tb").addClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $(" li.teacher .list-active .on").click(function () { 
+        // console.log("đã click")
+        $("li.teacher .tb").addClass("d-none");
+        $("li.teacher .list-active .btu").addClass("d-none");
+        $("li.teacher .list-active .add").removeClass("d-none");
+        $("li.teacher .list-active .x").removeClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $("li.teacher .list-active .x").click(function () { 
+        // console.log("đã click")
+        $("li.teacher .list-active .btu").removeClass("d-none");
+        $("li.teacher .list-active .tb").removeClass("d-none");
+        $("li.teacher .list-active .x").addClass("d-none");
+        $(" li.teacher .list-active .add").addClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $(" li.teacher .list-active .tw").click(function () { 
+        // console.log("đã click")
+        $("li.teacher .tb").addClass("d-none");
+        $("li.teacher .list-active .btu").addClass("d-none");
+        $("li.teacher .list-active .up").removeClass("d-none");
+        $("li.teacher .list-active .x").removeClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $("li.teacher .list-active .x").click(function () { 
+        // console.log("đã click")
+        $("li.teacher .list-active .btu").removeClass("d-none");
+        $("li.teacher .list-active .tb").removeClass("d-none");
+        $("li.teacher .list-active .x").addClass("d-none");
+        $(" li.teacher .list-active .up").addClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $(" li.teacher .list-active .thir").click(function () { 
+        // console.log("đã click")
+        $("li.teacher .tb").addClass("d-none");
+        $("li.teacher .list-active .btu").addClass("d-none");
+        $("li.teacher .list-active .del").removeClass("d-none");
+        $("li.teacher .list-active .x").removeClass("d-none");
+    });
+});
+$(document).ready(function () {
+    $("li.teacher .list-active .x").click(function () { 
+        // console.log("đã click")
+        $("li.teacher .list-active .btu").removeClass("d-none");
+        $("li.teacher .list-active .tb").removeClass("d-none");
+        $("li.teacher .list-active .x").addClass("d-none");
+        $(" li.teacher .list-active .del").addClass("d-none");
     });
 });
